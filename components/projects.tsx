d3df7ab65f0a7d5f6d15ea77e0161792364ba472
arataaq <mochammadfnz03@gmail.com>
2024-12-09 15:07:17 +0700

    Update product.tsx

--- components/projects.tsx
+++ components/projects.tsx
@@ -6,8 +6,8 @@ import { projectsData } from "@/lib/data";
 import Project from "./project";
 import { useSectionInView } from "@/lib/hooks";
 
-export default function Projects() {
-  const { ref } = useSectionInView("Projects", 0.5);
+export default function Product() {
+  const { ref } = useSectionInView("Product", 0.5);
 
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
