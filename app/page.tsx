f05d62f02157cd86cd65df9bea4064642dd5027c
arataaq <mochammadfnz03@gmail.com>
2024-12-09 14:54:17 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import About from "@/components/about";
-import Contact from "@/components/contact";
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
-import Skills from "@/components/skills";
 
 export default function Home() {
   return (
@@ -13,9 +11,7 @@ export default function Home() {
       <SectionDivider />
       <About />
       <Projects />
-      {/* <Skills /> */}
       <Experience />
-      {/* <Contact /> */}
     </main>
   );
 }
